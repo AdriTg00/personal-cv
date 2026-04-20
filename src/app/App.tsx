6dbbd8c96e172de8b6b61a113f53fc39a1a43996
Adrian <adrian2000tg@gmail.com>
2026-04-20 12:17:48 +0200

    Revert "Install Vercel Web Analytics"

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -18,7 +18,6 @@ import {
   Download,
   Award
 } from 'lucide-react';
-import { Analytics } from '@vercel/analytics/react';
 
 import fotoPerfil from "../assets/adrian.png";
 import championsImg from "../assets/CHAMPIONS.png";
@@ -788,7 +787,6 @@ export default function App() {
       <footer className="py-8 text-center text-gray-500 border-t border-white/10">
         <p>© 2026 Adrián Tarancón. Todos los derechos reservados.</p>
       </footer>
-      <Analytics />
     </div>
   );
 }
